668a868823a0a5a3d1bbb8673e14d78f6f0ab2d9
Sultan Alsawaf <sultan@kerneltoast.com>
2019-06-24 07:22:55 -0700

    simple_lmk: Introduce Simple Low Memory Killer for Android
    This is a complete low memory killer solution for Android that is small
    and simple. Processes are killed according to the priorities that
    Android gives them, so that the least important processes are always
    killed first. Processes are killed until memory deficits are satisfied,
    as observed from kswapd struggling to free up pages. Simple LMK stops
    killing processes when kswapd finally goes back to sleep.
    
    The only tunables are the desired amount of memory to be freed per
    reclaim event and desired frequency of reclaim events. Simple LMK tries
    to free at least the desired amount of memory per reclaim and waits
    until all of its victims' memory is freed before proceeding to kill more
    processes.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -50,6 +50,12 @@
 #include <linux/printk.h>
 #include <linux/dax.h>
 #include <linux/psi.h>
+#include <linux/pagevec.h>
+#include <linux/shmem_fs.h>
+#include <linux/ctype.h>
+#include <linux/debugfs.h>
+#include <linux/simple_lmk.h>
+
 
 #include <asm/tlbflush.h>
 #include <asm/div64.h>
@@ -3979,6 +3985,7 @@ restart:
 		bool balanced;
 		bool ret;
 
+		simple_lmk_decide_reclaim(sc.priority);
 		sc.reclaim_idx = classzone_idx;
 
 		/*
@@ -4173,6 +4180,7 @@ static void kswapd_try_to_sleep(pg_data_t *pgdat, int alloc_order, int reclaim_o
 	 * succeed.
 	 */
 	if (prepare_kswapd_sleep(pgdat, reclaim_order, classzone_idx)) {
+		simple_lmk_stop_reclaim();
 		/*
 		 * Compaction records what page blocks it recently failed to
 		 * isolate pages from and skips them in the future scanning.
@@ -4209,6 +4217,7 @@ static void kswapd_try_to_sleep(pg_data_t *pgdat, int alloc_order, int reclaim_o
 	 */
 	if (!remaining &&
 	    prepare_kswapd_sleep(pgdat, reclaim_order, classzone_idx)) {
+		simple_lmk_stop_reclaim();
 		trace_mm_vmscan_kswapd_sleep(pgdat->node_id);
 
 		/*
