79c67d4f2ea392f6e1848fd0dab163be19a63ca1
apass3rby <100168186+apass3rby@users.noreply.github.com>
2022-07-01 14:32:26 +0200

    sm8250: input: touchscreen: fts5cu56a: correct low power proximity reporting
    Change-Id: I2d261ca14906e96312fe0b23cb79585341c30931

--- drivers/input/touchscreen/stm/fts5cu56a/fts_ts.c
+++ drivers/input/touchscreen/stm/fts5cu56a/fts_ts.c
@@ -1807,10 +1807,11 @@ static u8 fts_event_handler_type_b(struct fts_ts_info *info)
 			if (p_event_status->stype == FTS_EVENT_STATUSTYPE_VENDORINFO) {
 				if (info->board->support_ear_detect) {
 					if (p_event_status->status_id == 0x6A) {
+						p_event_status->status_data_1 = p_event_status->status_data_1 == 5 || !p_event_status->status_data_1;
 						info->hover_event = p_event_status->status_data_1;
-						input_report_abs(info->input_dev_proximity, ABS_MT_CUSTOM, !p_event_status->status_data_1);
+						input_report_abs(info->input_dev_proximity, ABS_MT_CUSTOM, p_event_status->status_data_1);
 						input_sync(info->input_dev_proximity);
-						input_info(true, &info->client->dev, "%s: proximity: %d\n", __func__, !p_event_status->status_data_1);
+						input_info(true, &info->client->dev, "%s: proximity: %d\n", __func__, p_event_status->status_data_1);
 					}
 				}
 			}
