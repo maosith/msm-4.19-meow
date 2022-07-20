2299ba8e448533340c1c970c663eb892d4ea9621
Roman Birg <roman@cyngn.com>
2022-07-20 17:47:14 +0200

    input: keyboard: hall_ic: Report SW_LID instead of SW_FLIP
    * Android expects SW_LID for lid events.
    
    Change-Id: Iebffbabdbb3748eec4f887ebd227c67adf01d8ef | AOSP
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- drivers/input/keyboard/hall_ic.c
+++ drivers/input/keyboard/hall_ic.c
@@ -79,7 +79,7 @@ static void flip_cover_work(struct work_struct *work)
 
 	if (first == second) {
 		flip_cover = first;
-		input_report_switch(ddata->input, SW_FLIP, flip_cover);
+		input_report_switch(ddata->input, SW_LID, flip_cover);
 		input_sync(ddata->input);
 	}
 }
@@ -99,7 +99,7 @@ static void flip_cover_work(struct work_struct *work)
 
 	flip_cover = first;
 	input_report_switch(ddata->input,
-			SW_FLIP, ddata->flip_cover);
+			SW_LID, ddata->flip_cover);
 	input_sync(ddata->input);
 #else
 	ddata->flip_cover = !gpio_get_value(ddata->gpio_flip_cover_key1) & !gpio_get_value(ddata->gpio_flip_cover_key2);
@@ -111,7 +111,7 @@ static void flip_cover_work(struct work_struct *work)
 			ddata->emulated_hall_ic_status = !ddata->emulated_hall_ic_status;
 			pr_info("keys:%s #1 : %d , %d\n", __func__, first, ddata->emulated_hall_ic_status);	
 			input_report_switch(ddata->input,
-					SW_FLIP, ddata->emulated_hall_ic_status);
+					SW_LID, ddata->emulated_hall_ic_status);
 			input_sync(ddata->input);
 		}
 		flip_cover = first;
@@ -280,7 +280,7 @@ static int hall_probe(struct platform_device *pdev)
 	input->dev.parent = &pdev->dev;
 
 	input->evbit[0] |= BIT_MASK(EV_SW);
-	input_set_capability(input, EV_SW, SW_FLIP);
+	input_set_capability(input, EV_SW, SW_LID);
 
 	input->open = hall_open;
 	input->close = hall_close;
