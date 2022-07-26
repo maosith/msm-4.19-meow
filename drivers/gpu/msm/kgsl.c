f2b19e8b6e7e6afb661f1c3044a61ad2fda9b57e
Alexander Winkowski <dereference23@outlook.com>
2022-07-26 05:21:29 +0000

    kgsl: Run low_prio_worker on little CPUs
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5537,18 +5537,6 @@ static void kgsl_core_exit(void)
 		ARRAY_SIZE(kgsl_driver.devp));
 }
 
-static long kgsl_run_one_worker(struct kthread_worker *worker,
-		struct task_struct **thread, const char *name)
-{
-	kthread_init_worker(worker);
-	*thread = kthread_run(kthread_worker_fn, worker, name);
-	if (IS_ERR(*thread)) {
-		pr_err("unable to start %s\n", name);
-		return PTR_ERR(thread);
-	}
-	return 0;
-}
-
 static int __init kgsl_core_init(void)
 {
 	int result = 0;
@@ -5622,12 +5610,18 @@ static int __init kgsl_core_init(void)
 
 	INIT_WORK(&kgsl_driver.mem_work, _flush_mem_workqueue);
 
-	if (IS_ERR_VALUE(kgsl_run_one_worker(&kgsl_driver.worker,
-			&kgsl_driver.worker_thread,
-			"kgsl_worker_thread")) ||
-		IS_ERR_VALUE(kgsl_run_one_worker(&kgsl_driver.low_prio_worker,
-			&kgsl_driver.low_prio_worker_thread,
-			"kgsl_low_prio_worker_thread")))
+	kthread_init_worker(&kgsl_driver.worker);
+
+	kgsl_driver.worker_thread = kthread_run(kthread_worker_fn,
+		&kgsl_driver.worker, "kgsl_worker_thread");
+
+	kthread_init_worker(&kgsl_driver.low_prio_worker);
+
+	kgsl_driver.low_prio_worker_thread = kthread_run_perf_critical(cpu_lp_mask,
+		kthread_worker_fn, &kgsl_driver.low_prio_worker, "kgsl_low_prio_worker_thread");
+
+	if (IS_ERR_VALUE(kgsl_driver.worker_thread) ||
+		IS_ERR_VALUE(kgsl_driver.low_prio_worker_thread))
 		goto err;
 
 	sched_setscheduler(kgsl_driver.worker_thread, SCHED_FIFO, &param);
