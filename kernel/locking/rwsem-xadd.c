621296b65f5af75ec167e461f7796e3f4bb3084b
Samuel Pascua <pascua.samuel.14@gmail.com>
2023-10-06 10:46:14 +0800

    treewide: remove Samsung scheduler additions
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- kernel/locking/rwsem-xadd.c
+++ kernel/locking/rwsem-xadd.c
@@ -90,9 +90,6 @@ void __init_rwsem(struct rw_semaphore *sem, const char *name,
 	sem->owner = NULL;
 	osq_lock_init(&sem->osq);
 #endif
-#ifdef CONFIG_FAST_TRACK
-	sem->ftt_dep_task = NULL;
-#endif
 #ifdef CONFIG_RWSEM_PRIO_AWARE
 	sem->m_count = 0;
 #endif
@@ -271,10 +268,6 @@ __rwsem_down_read_failed_common(struct rw_semaphore *sem, int state)
 	     (adjustment != -RWSEM_ACTIVE_READ_BIAS ||
 	     is_first_waiter)))
 		__rwsem_mark_wake(sem, RWSEM_WAKE_ANY, &wake_q);
-
-#ifdef CONFIG_FAST_TRACK
-	rwsem_dynamic_ftt_enqueue(current, waiter.task, READ_ONCE(sem->owner), sem);
-#endif
 
 	raw_spin_unlock_irq(&sem->wait_lock);
 	wake_up_q(&wake_q);
@@ -581,10 +574,6 @@ __rwsem_down_write_failed_common(struct rw_semaphore *sem, int state)
 	} else
 		count = atomic_long_add_return(RWSEM_WAITING_BIAS, &sem->count);
 
-#ifdef CONFIG_FAST_TRACK
-	rwsem_dynamic_ftt_enqueue(waiter.task, current, READ_ONCE(sem->owner), sem);
-#endif
-
 	/* wait until we successfully acquire the lock */
 	set_current_state(state);
 	while (true) {
@@ -709,10 +698,6 @@ locked:
 
 	if (!list_empty(&sem->wait_list))
 		__rwsem_mark_wake(sem, RWSEM_WAKE_ANY, &wake_q);
-
-#ifdef CONFIG_FAST_TRACK
-	rwsem_dynamic_ftt_dequeue(sem, current);
-#endif
 
 	raw_spin_unlock_irqrestore(&sem->wait_lock, flags);
 	wake_up_q(&wake_q);
