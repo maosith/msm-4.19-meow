3499a8cfb7cbad4717cdadadc74dc2ec8fe29b7a
root <root@DESKTOP-184HKS5.localdomain>
2026-06-28 10:58:23 -0300

    build_env: Fix VDSO fucking around

--- meow.sh
+++ meow.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-LLVM_PATH="$HOME/clang-r510928/bin/"
+export PATH="/usr/lib/ccache:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+LLVM_PATH="/usr/bin"
 
 read -p "Digite um nome para o kernel: " KERNEL_NAME
 
@@ -9,19 +11,29 @@ if [ -z "$KERNEL_NAME" ]; then
     exit 1
 fi
 
-TC_PATH="$HOME/clang-r510928/bin/"
+TC_PATH="/usr/bin/"
 GCC_PATH="/usr/bin/"
-
-BUILD_ENV="CC=${TC_PATH}clang CROSS_COMPILE=${GCC_PATH}aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 PATH=$LLVM_PATH:$LLD_PATH:$PATH"  
+BUILD_ENV="CC=${TC_PATH}clang \
+LD=${TC_PATH}ld.lld \
+AR=${TC_PATH}llvm-ar \
+NM=${TC_PATH}llvm-nm \
+OBJCOPY=${TC_PATH}llvm-objcopy \
+OBJDUMP=${TC_PATH}llvm-objdump \
+STRIP=${TC_PATH}llvm-strip \
+CROSS_COMPILE=${GCC_PATH}aarch64-linux-gnu- \
+CROSS_COMPILE_ARM32=${GCC_PATH}arm-linux-gnueabi- \
+LLVM=1 LLVM_IAS=1 PATH=$LLVM_PATH:$PATH"  
 
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
+mkdir -p out
 make O=out ARCH=arm64 $BUILD_ENV r8q_defconfig
+make O=out ARCH=arm64 $BUILD_ENV prepare
+make O=out ARCH=arm64 $BUILD_ENV vdso_prepare
 
 DATE_START=$(date +"%s")
 
 make -j$(nproc --all) O=out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV Image.gz
-
 make -j$(nproc --all) O=out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV dtbs
 
 DTB_OUT="out/arch/arm64/boot/dts/vendor/qcom"
@@ -35,6 +47,5 @@ echo "Tempo de compilação: $(($DIFF / 60)) minutos(s) and $(($DIFF % 60)) segu
 
 cp $IMAGE AnyKernel3/Image.gz
 cd AnyKernel3
-rm *.zip
-zip -r9 ${KERNEL_NAME}-.zip .
-
+rm -f *.zip # O -f impede o script de travar caso não existam zips antigos
+zip -r9 ${KERNEL_NAME}.zip .
